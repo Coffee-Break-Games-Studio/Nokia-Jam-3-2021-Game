ea97c8a3a810514871eea4d8a463807f30468b47
lazyannie <43974954+lazyannie@users.noreply.github.com>
2021-02-08 16:20:04 -0500

    Timer Bar

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    Image timerBar;
+    public float maxTime = 5f;
+    float timeLeft;
+
+
+    void Start()
+    {
+        timerBar = GetComponent<Image>();
+        timeLeft = maxTime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timeLeft > 0)
+        {
+            timeLeft -= Time.deltaTime;
+            timerBar.fillAmount = timeLeft / maxTime;
+        }
+        else
+        {
+            Loader.Load(Loader.Scene.GameOverScene);
+        }
+    }
+}
