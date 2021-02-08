a46a3d9345dc8095b1595f4a5fb702035f211aa8
Jassiel Llerena <jazy@jazyserver.com>
2021-02-08 01:59:54 -0500

    Create audio controller logic

---
+++ Assets/Scripts/AudioController.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioController : MonoBehaviour
+{
+
+    public AudioSource audioSource;
+    Coroutine playingRoutine = null;
+    AudioClip interruptedClip = null;
+    float interruptedClipTime = 0;
+
+    // Plays the provided audio clip
+    public void Play(AudioClip clip)
+    {
+      audioSource.Stop();
+      audioSource.clip = clip;
+      audioSource.Play();
+    }
+
+    // Plays the clip once and then resumes what was being played before
+    public void PlayOneShot(AudioClip clip)
+    {
+      if (playingRoutine != null) {
+        StopCoroutine(playingRoutine);
+      }
+      playingRoutine = StartCoroutine(_playOneShot(clip));
+    }
+
+    IEnumerator _playOneShot(AudioClip clip)
+    {
+      if (interruptedClip == null) {
+        interruptedClip = audioSource.clip;
+        interruptedClipTime = audioSource.time;
+      }
+
+      audioSource.Stop();
+      audioSource.clip = clip;
+      audioSource.Play();
+      yield return new WaitWhile(() => audioSource.isPlaying);
+      audioSource.clip = interruptedClip;
+      audioSource.Play();
+
+      interruptedClip = null;
+      playingRoutine = null;
+    }
+
+    // singleton
+    void Awake()
+    {
+      GameObject[] objs = GameObject.FindGameObjectsWithTag("AudioController");
+      if (objs.Length > 1) {
+        Destroy(this.gameObject); // seppuku
+      }
+      DontDestroyOnLoad(this.gameObject);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}   
