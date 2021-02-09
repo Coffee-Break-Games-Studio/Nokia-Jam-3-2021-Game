88abaf677e594959416033ae0fc2f0ec40c298cf
Jassiel Llerena <jazy@jazyserver.com>
2021-02-09 15:46:01 -0500

    Fix game over scene
    i somehow managed to yet the gameover controller and replace it with the victory script...

--- Assets/Scripts/GameOverController.cs
+++ Assets/Scripts/GameOverController.cs
@@ -4,18 +4,29 @@ using UnityEngine;
 
 public class GameOverController : MonoBehaviour
 {
+    public AudioClip ButtonPressAudio;
+    AudioController audioController;
 
+    public void OnContinueSelected()
+    {
+        audioController.Play(ButtonPressAudio);
+        Loader.Load(Loader.Scene.IntroScene);
+    }
+
+    public void OnQuitSelected()
+    {
+        audioController.Play(ButtonPressAudio);
+        Application.Quit();
+    }
     // Start is called before the first frame update
     void Start()
     {
-
+        audioController = GameObject.FindGameObjectWithTag("AudioController").GetComponent<AudioController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetButtonDown("Action")) {
-          Loader.Load(Loader.Scene.IntroScene);
-        }
+
     }
 }
