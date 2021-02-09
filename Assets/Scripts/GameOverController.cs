1f5112097389b4f5a676834f53b823e326a05ead
Jassiel Llerena <jazy@jazyserver.com>
2021-02-09 15:37:36 -0500

    Fix game over controller script

--- Assets/Scripts/GameOverController.cs
+++ Assets/Scripts/GameOverController.cs
@@ -4,21 +4,17 @@ using UnityEngine;
 
 public class GameOverController : MonoBehaviour
 {
-    public AudioClip EndSong;
-    AudioController audioController;
 
     // Start is called before the first frame update
     void Start()
     {
-        audioController = GameObject.FindGameObjectWithTag("AudioController").GetComponent<AudioController>();
-        audioController.Play(EndSong);
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetButton("Action")) {
-          IntroSceneController.IntroSceneVisisted = false;
+        if (Input.GetButtonDown("Action")) {
           Loader.Load(Loader.Scene.IntroScene);
         }
     }
